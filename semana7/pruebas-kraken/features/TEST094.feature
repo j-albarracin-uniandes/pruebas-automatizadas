5501c78ef1699777582857371241ba93f8363b53
Franz Rogelez <frogelez@zenware.com.co>
2022-05-20 17:51:10 -0500

    Semana 7

---
+++ semana7/pruebas-kraken/features/TEST094.feature
@@ -0,0 +1,27 @@
+Feature: POST
+
+@user4 @web
+Scenario: TEST091
+    Given I navigate to page "<LOGIN_URL>"
+    And I wait for 1 seconds
+    And I set text "<USERNAME>" on element with xpath "<USERNAME_EID>"
+    And I set text "<PASSWORD>" on element with xpath "<PASSWORD_EID>"
+    And I wait for 1 seconds
+    Then I click element with xpath "<BTN_SIGNIN_EID>"
+    And I wait for 1 seconds
+    Given I navigate to page "<CREATE_POST_URL>"
+    And I wait for 1 seconds
+    And I set text "$string_1" on element with xpath "<POST_TITLE_ECLASS>"
+    And I wait for 1 seconds
+    And I set text "$$string_1" on element with xpath "<CONTENT_ECLASS>"
+    And Enter on element with xpath "<CONTENT_ECLASS>"
+    And I set text "$string_3" on element with xpath "<CONTENT_ECLASS>"
+    And I wait for 1 seconds
+    Then I click element with xpath "<OPEN_SETTINGS_CLASS>"
+    And I set text "$url_1" on element with xpath "<POST_URL_XPATH>"
+    And I wait for 1 seconds
+    And I set text "$string_3" on element with xpath "<CONTENT_ECLASS>"
+    And I wait for 1 seconds
+	And I set text "$string_1" on element with xpath "<FILTER_VIEW_INPUT_TAG_XPATH>"
+	And Enter on element with xpath "<FILTER_VIEW_INPUT_TAG_XPATH>"
+	And I wait for 5 seconds
--- semana7/pruebas-kraken/features/web/step_definitions/step.js
+++ semana7/pruebas-kraken/features/web/step_definitions/step.js
@@ -1,5 +1,6 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
+
 When('Enter on element with xpath {kraken-string}', async function(xpath) {
     let element = await this.driver.$(xpath);
     return await element.setValue("\n");
@@ -14,3 +15,37 @@ Then('I set text {kraken-string} on element with xpath {kraken-string}', async f
     let view = await this.driver.$(xpath);
     return await view.setValue(input);
 });
+
+When('I test pseudo data on element with xpath {kraken-string} with error {kraken-string} on file {kraken-string} expected errors', async function (xpath, errorXpath, file) {
+    let data = require(file);
+    var mydata = JSON.parse(JSON.stringify(data));  
+    
+    for (let item of mydata) {   
+        console.log(item);     
+        let element = await this.driver.$(xpath);
+        await element.setValue(item.data);
+        await element.setValue("\n");
+        if(!await this.driver.$(errorXpath)){
+            throw "El resultado no fue lo esperado";
+        }
+    }
+    return true;
+});
+
+When('I test pseudo data on element with xpath {kraken-string} with error {kraken-string} on file {kraken-string} expected success', async function (xpath, errorXpath, file) {
+    let data = require(file);
+    var mydata = JSON.parse(JSON.stringify(data));  
+    
+    for (let item of mydata) {   
+        console.log(item);     
+        let element = await this.driver.$(xpath);
+        await element.setValue(item.data);
+        await element.setValue("\n");
+        if(await this.driver.$(errorXpath)){
+            throw "El resultado no fue lo esperado";
+        }
+    }
+    return true;
+});
+
+
