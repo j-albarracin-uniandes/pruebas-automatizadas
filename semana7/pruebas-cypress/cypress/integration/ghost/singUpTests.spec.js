d9d0b5237b580cd7f8174bb37658d9818d0cbc61
wilber.palomino <w.palomino@uniandes.edu.co>
2022-05-22 09:18:28 -0500

    pruebas de las funcionalidades login, singup y members

--- semana7/pruebas-cypress/cypress/integration/ghost/singUpTests.spec.js
+++ semana7/pruebas-cypress/cypress/integration/ghost/singUpTests.spec.js
@@ -26,11 +26,7 @@ context("Actions", () => {
     cy.get("footer").find("li.nav-sign-up").click();
     cy.wait(1000);
     getIframeBody().find(gC.signin.selectors.submit).should('have.text', 'Sign up').click()
-    // cy.get("#ghost-portal-root > div > iframe").find("h1.gh-portal-main-title").should('be.visible');
-    // cy.get('#signInFormUsername', { timeout: 10000 }).eq(0).click({ force: true})
-    // cy.get('body.home-template > div#ghost-portal-root').find('button').should('be.visible');
-    // cy.get(".gh-portal-popup-wrapper").click();
-    // cy.get(".error").find(gC.signin.selectors.email).should("have.length", 1);
+    getIframeBody().find("input.error").first().should("have.length", 1);
   });
 
   
